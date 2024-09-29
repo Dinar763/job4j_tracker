97a754f38a3877df480c358f44e341b1707e17df
Dinar <gizzyatovdinar@gmail.com>
2024-09-29 18:58:13 +0400

    Репозиторий под проект Tracker [14]

--- src/test/java/ru/job4j/tracker/TrackerTest.java
+++ src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -6,11 +6,5 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class TrackerTest {
     @Test
     public void whenAddNewItemThenTrackerHasSameItem() {
-        Tracker tracker = new Tracker();
-        Item item = new Item();
-        item.setName("test1");
-        tracker.add(item);
-        Item result = tracker.findById(item.getId());
-        assertThat(result.getName()).isEqualTo(item.getName());
     }
 }
