8e6b24384542b37118d44081bd4be732c7a5707c
Dinar <gizzyatovdinar@gmail.com>
2024-10-10 23:35:00 +0400

    Репозиторий под проект Tracker [31]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -9,6 +11,22 @@ public class Tracker {
         item.setId(ids++);
         items[size++] = item;
         return item;
+    }
+
+    public Item[] findAll() {
+        return Arrays.copyOf(items, size);
+    }
+
+    public Item[] findByName(String key) {
+        Item[] itemsNew = new Item[items.length];
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
+            if (items[i].getName().equals(key)) {
+                itemsNew[counter] = items[i];
+                counter++;
+            }
+        }
+        return Arrays.copyOf(itemsNew, counter);
     }
 
     public Item findById(int id) {
