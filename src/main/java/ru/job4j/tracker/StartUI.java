748714005979533ba80fa8d6eb58cc19e3dc28e9
Dinar <gizzyatovdinar@gmail.com>
2024-10-19 20:47:05 +0400

    Репозиторий под проект Tracker [40]

---
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -0,0 +1,8 @@
+package ru.job4j.tracker;
+
+public class StartUI {
+    public static void main(String[] args) {
+        Item items = new Item(314, "Test");
+        System.out.println(items.toString());
+    }
+}
