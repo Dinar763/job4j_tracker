da22503998089c66ec85e66ad821d0e16efa3f7e
Dinar <gizzyatovdinar@gmail.com>
2024-10-19 09:16:51 +0400

    Репозиторий под проект Tracker [38]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -57,11 +57,8 @@ public class Tracker {
 
     public void delete(int id) {
         int index = indexOf(id);
-        int start = index + 1;
-        int distPos = index;
-        int length = size - index - 1;
-        if (id <= 100) {
-            System.arraycopy(items, start, items, distPos, length);
+        if (index != -1) {
+            System.arraycopy(items, index + 1, items, index, size - index - 1);
             items[size - 1] = null;
             size--;
         }
