012a863f99b1d1866ea00c51f1344f361233d1bb
Dinar <gizzyatovdinar@gmail.com>
2024-10-31 10:18:59 +0400

    Репозиторий под проект Tracker [53]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -66,7 +66,7 @@ public class StartUI {
                         System.out.println(item);
                     }
                 } else {
-                    System.out.println("Заявки с именем: " + name + " не найдены.");
+                    System.out.println("Заявки с введенным именем: " + name + " не найдены.");
                 }
             } else if (select == 6) {
                 run = false;
