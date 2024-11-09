8e155036a52e3cb511e4b3fe5c3c9a3ee4cf4fed
Dinar <gizzyatovdinar@gmail.com>
2024-11-09 20:20:27 +0400

    Репозиторий под проект Tracker [55]

--- src/main/java/ru/job4j/oop/Student.java
+++ src/main/java/ru/job4j/oop/Student.java
@@ -13,10 +13,4 @@ public class Student {
     public void sing() {
         System.out.println("I believe I can fly");
     }
-
-    public static void main(String[] args) {
-        Student petya = new Student();
-        String song = "I believe, I can fly";
-        petya.music(song);
-    }
 }
