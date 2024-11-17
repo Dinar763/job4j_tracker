e9c1871993413bd7b2efbdfdd0faa76251984d5c
Dinar <gizzyatovdinar@gmail.com>
2024-11-17 10:39:42 +0400

    Репозиторий под проект Tracker [62]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -38,12 +38,9 @@ class StartUITest {
         Tracker tracker = new Tracker();
         Item item = new Item("new item");
         tracker.add(item);
-        String[] answers = {
-            String.valueOf(item.getId()),
-            "edited item"
-        };
+        String[] answers = {String.valueOf(item.getId())};
         StartUI.deleteItem(new MockInput(answers), tracker);
         Item edited = tracker.findById(item.getId());
-        assertThat(edited == null);
+        assertThat(edited).isNull();
     }
 }
