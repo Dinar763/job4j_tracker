b145b101501e535a3047c3bd1599d8cef8247646
Dinar <gizzyatovdinar@gmail.com>
2024-11-24 13:28:13 +0400

    Репозиторий под проект Tracker [63]

--- src/main/java/ru/job4j/tracker/FindAllAction.java
+++ src/main/java/ru/job4j/tracker/FindAllAction.java
@@ -3,11 +3,20 @@ package ru.job4j.tracker;
 public class FindAllAction implements UserAction {
     @Override
     public String name() {
-        return null;
+        return "Показать все заявки";
     }
 
     @Override
     public boolean execute(Input input, Tracker tracker) {
-        return false;
+        System.out.println("=== Список всех заявок ===");
+        Item[] items = tracker.findAll();
+        if (items.length > 0) {
+            for (Item item : items) {
+                System.out.println(item);
+            }
+        } else {
+            System.out.println("Хранилище еще не содержит заявок");
+        }
+        return true;
     }
 }
