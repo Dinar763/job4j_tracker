6eeeaec923e86575b692c038408333f2220bc409
Dinar <gizzyatovdinar@gmail.com>
2024-11-28 23:18:47 +0400

    Репозиторий под проект Tracker [74]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,9 +1,24 @@
 package ru.job4j.ex;
 
 public class FindEl {
-    public static int indexOf(String[] value, String key) {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
         int result = -1;
-        /* цикл fori, поскольку нам надо найти индекс искомого элемента в массиве */
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                result = i;
+            }
+        }
+        if (result == -1) {
+            throw new ElementNotFoundException("Элемент не найден");
+        }
         return result;
+    }
+
+    public static void main(String[] args) {
+        try {
+            indexOf(new String[]{"nananan hhfhdhd", "dkdkdjf", "skskskskp"}, "ferer");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
     }
 }
