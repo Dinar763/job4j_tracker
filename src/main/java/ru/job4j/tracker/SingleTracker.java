3c961a878c5b0ab62e43760942b184855cff9219
Dinar <gizzyatovdinar@gmail.com>
2024-12-06 00:25:28 +0400

    Репозиторий под проект Tracker [82]

--- src/main/java/ru/job4j/tracker/SingleTracker.java
+++ src/main/java/ru/job4j/tracker/SingleTracker.java
@@ -2,15 +2,16 @@ package ru.job4j.tracker;
 
 public class SingleTracker {
 
-    private Tracker tracker = null;
+    private static Tracker tracker = new Tracker();
 
     private SingleTracker() {
     }
 
-    public SingleTracker(Tracker tracker) {
+    public static Tracker getTracker() {
         if (tracker == null) {
             tracker = new Tracker();
         }
+        return tracker;
     }
 
     public Item add(Item item) {
