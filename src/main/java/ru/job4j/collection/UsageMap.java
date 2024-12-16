4900083329e6ed5d0d2e4abb01e4d5014df90b61
Dinar <gizzyatovdinar@gmail.com>
2024-12-16 10:42:52 +0400

    Репозиторий под проект Tracker [100]

--- src/main/java/ru/job4j/collection/UsageMap.java
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -7,6 +7,8 @@ public class UsageMap {
     public static void main(String[] args) {
         HashMap<String, String> map = new HashMap<>();
         map.put("petrov@mail.ru", "Петров Петр Петрович");
+        map.put("ivanov@mail.ru", "Ivanov Петр Петрович");
+        map.put("ionov@mail.ru", "Ionov Петр Петрович");
         for (Map.Entry<String, String> entry: map.entrySet()) {
             String key = entry.getKey();
             String value = entry.getValue();
