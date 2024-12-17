cc8b1fa7d7150d257ff40aac8d47de79d3f61a01
Dinar <gizzyatovdinar@gmail.com>
2024-12-18 01:34:00 +0400

    Репозиторий под проект Tracker [103]

---
+++ src/main/java/ru/job4j/hasmap/AnalyzeByMap.java
@@ -0,0 +1,107 @@
+package ru.job4j.hasmap;
+
+import java.util.*;
+
+public class AnalyzeByMap {
+    public static double averageScore(List<Pupil> pupils) {
+        double sumScore = 0D;
+        int counterSubjects = 0;
+        for (Pupil pupil: pupils) {
+            for (Subject subject: pupil.subjects()) {
+                sumScore += subject.score();
+                counterSubjects++;
+            }
+        }
+        return counterSubjects == 0 ? 0D : sumScore / counterSubjects;
+    }
+
+    public static List<Label> averageScoreByPupil(List<Pupil> pupils) {
+        List<Label> listOfLabel = new ArrayList<>();
+        double sumScore = 0D;
+        int counterSubjects = 0;
+        for (Pupil pupil: pupils) {
+            for (Subject subject: pupil.subjects()) {
+                sumScore += subject.score();
+                counterSubjects++;
+            }
+            listOfLabel.add(new Label(pupil.name(), sumScore / counterSubjects));
+            sumScore = 0;
+            counterSubjects = 0;
+        }
+        return listOfLabel;
+    }
+
+    public static List<Label> averageScoreBySubject(List<Pupil> pupils) {
+        Map<String, Integer> map = new HashMap<>();
+        Set<String> setOfName = new HashSet<>();
+        for (Pupil pupil: pupils) {
+            for (Subject subject: pupil.subjects()) {
+                map.put(subject.name(), map.getOrDefault(subject.name(), 0) + subject.score());
+            }
+            setOfName.add(pupil.name());
+        }
+        List<Label> listOfLabel = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            listOfLabel.add(new Label(entry.getKey(), entry.getValue() / (double) setOfName.size()));
+        }
+        return listOfLabel;
+    }
+
+    public static Label bestStudent(List<Pupil> pupils) {
+        List<Label> listOfLabel = new ArrayList<>();
+        double sumScore = 0D;
+        for (Pupil pupil: pupils) {
+            for (Subject subject: pupil.subjects()) {
+                sumScore += subject.score();
+            }
+            listOfLabel.add(new Label(pupil.name(), sumScore));
+            sumScore = 0;
+        }
+        listOfLabel.sort(Comparator.naturalOrder());
+        return listOfLabel.get(listOfLabel.size() - 1);
+    }
+
+    public static Label bestSubject(List<Pupil> pupils) {
+        Map<String, Integer> map = new HashMap<>();
+        for (Pupil pupil: pupils) {
+            for (Subject subject: pupil.subjects()) {
+                map.put(subject.name(), map.getOrDefault(subject.name(), 0) + subject.score());
+            }
+        }
+        List<Label> listOfLabel = new ArrayList<>();
+        for (Map.Entry<String, Integer> entry: map.entrySet()) {
+            listOfLabel.add(new Label(entry.getKey(), entry.getValue()));
+        }
+        listOfLabel.sort(Comparator.naturalOrder());
+        return listOfLabel.get(listOfLabel.size() - 1);
+    }
+
+    public static void main(String[] args) {
+        List<Label> average = AnalyzeByMap.averageScoreByPupil(
+            List.of(
+                new Pupil("Ivanov",
+                    List.of(
+                        new Subject("Math", 100),
+                        new Subject("Lang", 60),
+                        new Subject("Philosophy", 80)
+                    )
+                ),
+                new Pupil("Petrov",
+                    List.of(
+                        new Subject("Math", 80),
+                        new Subject("Lang", 90),
+                        new Subject("Philosophy", 70)
+                    )
+                ),
+                new Pupil("Sidorov",
+                    List.of(
+                        new Subject("Math", 70),
+                        new Subject("Lang", 60),
+                        new Subject("Philosophy", 50)
+                    )
+                )
+            )
+        );
+        System.out.println(average.size());
+    }
+}
---
+++ src/main/java/ru/job4j/hasmap/Subject.java
@@ -0,0 +1,4 @@
+package ru.job4j.hasmap;
+
+public record Subject(String name, int score) {
+}
