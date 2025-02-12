8adc824fd78de2515c9d193e0a157ba3ca8d080d
Dinar <gizzyatovdinar@gmail.com>
2025-02-12 06:56:39 +0400

    Репозиторий под проект Tracker [137]

--- src/main/java/ru/job4j/iterator/MatrixIterator.java
+++ src/main/java/ru/job4j/iterator/MatrixIterator.java
@@ -16,6 +16,7 @@ public class MatrixIterator implements Iterator<Integer> {
     public boolean hasNext() {
         while (row < data.length && data[row].length == 0) {
             row++;
+            column = 0;
         }
         return row < data.length;
     }
@@ -25,12 +26,21 @@ public class MatrixIterator implements Iterator<Integer> {
         if (!hasNext()) {
             throw new NoSuchElementException();
         }
-        int val = data[row][column];
-        column++;
-        if (column >= data[row].length) {
+        int val = data[row][column++];
+        if (column == data[row].length) {
             row++;
             column = 0;
         }
         return val;
+    }
+
+    public static void main(String[] args) {
+        int[][] input = {
+            {1, 2},
+            {3, 4}
+        };
+        MatrixIterator iterator = new MatrixIterator(input);
+        System.out.println(iterator.next());
+        System.out.println(iterator.next());
     }
 }
